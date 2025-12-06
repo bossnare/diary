a27eae11e5943396438e66273e0753b612b59bdb
bossnare <razafimangagervaischristo@gmail.com>
2025-12-06 17:34:41 +0300

    add server with bun serve, and api routes (CRUD)

---
+++ apps/server/src/db/schema.ts
@@ -0,0 +1,28 @@
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
+
+export const users = sqliteTable('users', {
+  id: text('id').primaryKey(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  username: text('username').notNull(),
+  role: text('role').notNull().default('user'),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+});
+
+export const notes = sqliteTable('notes', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(CURRENT_TIMESTAMP)`),
+});
---
+++ apps/server/src/server.ts
@@ -0,0 +1,70 @@
+import { serve } from 'bun';
+import { db } from './db';
+import { notes, users } from './db/schema';
+import { eq } from 'drizzle-orm';
+import { v4 as uuid } from 'uuid';
+
+const server = serve({
+  port: 5000,
+  routes: {
+    '/': new Response('Api page #Onlinotes.'),
+
+    // users routes
+    '/api/users': async (req) => {
+      const all = await db.select().from(users);
+      return Response.json(all);
+    },
+    '/api/users/register': {
+      async POST(req) {
+        const body = '';
+        return Response.json({
+          ok: true,
+        });
+      },
+    },
+    '/api/users/me': async (req) => {
+      return Response.json({});
+    },
+    '/api/users/:id': async (req) => {
+      const id = req.params.id;
+      const usersById = await db.select().from(users).where(eq(notes.id, id));
+      return Response.json(usersById);
+    },
+    '/api/users/update/:id': {
+      async PUT(req) {
+        const id = req.params.id;
+        return Response.json({});
+      },
+    },
+
+    // notes routes
+    '/api/notes': async (req) => {
+      return Response.json({});
+    },
+    '/api/notes/create': {
+      async PUT(req) {
+        const body = '';
+        await db.insert(notes).values({
+          id: uuid(),
+          title: '',
+          content: '',
+        });
+        return Response.json({});
+      },
+    },
+    '/api/notes/:id': async (req) => {
+      const id = req.params.id;
+      return Response.json({
+        id,
+      });
+    },
+    '/api/notes/update/:id': {
+      async PUT(req) {
+        const id = req.params.id;
+        return Response.json({});
+      },
+    },
+  },
+});
+
+console.log(`Api run on http://localhost:${server.port}`);
