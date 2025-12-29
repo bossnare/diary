11094a7accb6d9ffaee32ff3efc5e0b76b877ea2
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2025-12-29 22:01:35 +0300

    Adjust animation and styling in NoteEditor component

--- apps/client/src/app/components/users/NoteEditor.tsx
+++ apps/client/src/app/components/users/NoteEditor.tsx
@@ -19,7 +19,7 @@ export const NoteEditor = ({
     <>
       {open && (
         <motion.div
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 100 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 20 }}
           className="fixed top-0 left-0 bg-background size-full lg:w-[calc(100%-16rem)] z-999 lg:absolute lg:left-64 lg:top-14"
@@ -33,17 +33,17 @@ export const NoteEditor = ({
             </Button>
           </nav>
 
-          <main className="flex flex-col h-full px-2 py-2 space-y-2 lg:px-4">
+          <main className="flex flex-col h-full px-2 py-2 space-y-4 lg:px-4">
             <input
               type="text"
-              className="text-xl placeholder:text-xl focus:outline-0"
+              className="text-2xl font-bold placeholder:text-xl focus:outline-0"
               placeholder="Title"
             />
             <textarea
               ref={areaRef}
               name=""
               id=""
-              className="w-full h-full resize-none focus:outline-0"
+              className="w-full h-[calc(100%-4rem)] font-medium text-lg resize-none focus:outline-0"
               placeholder="Start typing freely"
             ></textarea>
           </main>
