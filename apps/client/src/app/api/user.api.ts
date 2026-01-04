85fca3f4fff2f3508d70c90f4c87d7173d7b0742
Christo Razafimanga <144788145+bossnare@users.noreply.github.com>
2026-01-04 22:54:07 +0300

    Adjust cache and stale time for useMe query

--- apps/client/src/app/api/user.api.ts
+++ apps/client/src/app/api/user.api.ts
@@ -11,8 +11,9 @@ export function useMe() {
   return useQuery<NoteResponse>({
     queryKey: ['notes'],
     queryFn: async () => fetcher('/notes'),
-    staleTime: 0,
-    gcTime: 2 * 60 * 100, // 2min
+    staleTime: 5 * 1000, // 5 sec
+    cacheTime: 1000 * 60 * 5, // 5 min
+    gcTime: 2 * 60 * 100, // 2 min
     refetchOnWindowFocus: true,
   });
 }
