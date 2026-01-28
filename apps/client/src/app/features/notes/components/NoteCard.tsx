51154ce3339f01ae06205a56b34b30ec892d5946
bossnare <razafimangagervaischristo@gmail.com>
2026-01-28 14:23:40 +0300

    client:fix: NoteCard wrapper

--- apps/client/src/app/features/notes/components/NoteCard.tsx
+++ apps/client/src/app/features/notes/components/NoteCard.tsx
@@ -7,11 +7,11 @@ type Props = React.HTMLAttributes<HTMLDivElement> & { note: NoteInterface };
 export const NoteCard = ({ className, children, note, ...props }: Props) => {
   return (
     <div
-      {...props}
       className={cn(
         'relative flex flex-col font-inter gap-4 p-4 transition cursor-pointer select-none bg-card group active:scale-99 lg:active:scale-100 dark:shadow-none hover:bg-background/80 dark:hover:bg-muted active:opacity-60 dark:bg-muted/80 lg:shadow-sm rounded-2xl lg:rounded-xl',
         className
       )}
+      {...props}
     >
       <span className="text-lg w-[90%] font-bold leading-none truncate md:text-base line-clamp-2 lg:line-clamp-1 text-wrap">
         {note.title || 'Untitled'}
