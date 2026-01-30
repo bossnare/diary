1ea4ac07eba5865c5b0d50025f6b21f8ccbe5bc4
bossnare <razafimangagervaischristo@gmail.com>
2026-01-30 20:52:03 +0300

    client: use app user profiles on UserAvatar

--- apps/client/src/app/features/users/MiniProfile.tsx
+++ apps/client/src/app/features/users/MiniProfile.tsx
@@ -1,22 +1,20 @@
-import { useAuth } from '@/shared/hooks/use-auth';
 import { UserAvatar } from './UserAvatar';
 import { cn } from '@/app/lib/utils';
+import { useUser } from '@/app/hooks/use-user';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
 
 export const MiniProfile = ({ className }: Props) => {
-  const { user } = useAuth();
+  const { data: user } = useUser();
 
   return (
     <div className={cn('flex items-center select-none gap-3 mb-4', className)}>
       <UserAvatar user={user} />
       <div className="flex flex-col -space-y-1 overflow-hidden">
         <span className="text-lg font-bold tracking-tight truncate md:text-base line-clamp-1">
-          {user?.user_metadata.name.split(' (')[0] || 'User Diary'}
+          {user?.displayName.split(' (')[0] || 'User Diary'}
         </span>
-        <span className="text-sm text-muted-foreground">
-          {user?.user_metadata.email_verified && 'View your profile'}
-        </span>
+        <span className="text-sm text-muted-foreground">View your profile</span>
       </div>
     </div>
   );
