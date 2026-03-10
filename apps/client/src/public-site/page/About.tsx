be6af5a41415294c87e8a08324622bdbbd43a316
bossnare <razafimangagervaischristo@gmail.com>
2026-03-10 11:59:30 +0300

    client:public: add about content page

--- apps/client/src/public-site/page/About.tsx
+++ apps/client/src/public-site/page/About.tsx
@@ -1,7 +1,141 @@
 export const About = () => {
   return (
-    <div id="about" className="pt-16 px-6 text-center md:px-10 font-medium">
-      No content yet.
+    <div
+      id="about"
+      className="max-w-5xl px-6 pt-20 pb-40 mx-auto space-y-4 font-medium [&_section]:pb-4 prose divide-y dark:prose-invert md:px-0"
+    >
+      <section>
+        <h3>About Diary</h3>
+        <p>
+          <b className="font-semibold text-inter text-foreground">Diary</b> is a
+          simple application designed to help people{' '}
+          <b className="font-semibold text-inter text-foreground">
+            write, organize, and share their thoughts, history.
+          </b>{' '}
+          It allows users to create personal notes or diary entries, keep them{' '}
+          <b className="font-semibold text-inter text-foreground">private</b>,
+          or{' '}
+          <b className="font-semibold text-inter text-foreground">
+            publish them
+          </b>{' '}
+          for others in the community to read
+        </p>
+        <p>
+          The goal of Diary is to provide a{' '}
+          <b className="font-semibold text-inter text-foreground">
+            calm and simple space
+          </b>{' '}
+          where ideas, memories, and daily reflections can be written and
+          preserved.
+        </p>
+      </section>
+
+      <section>
+        <h3>What You Can Do</h3>
+        <ul>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Write Notes
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Organize Your Thoughts
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Keep Notes Private
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Publish to the Community
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Discover Stories
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Our Vision</h3>
+        <p>
+          Diary aims to create a
+          <b className="font-semibold text-inter text-foreground">
+            digital space for ideas and reflections.
+          </b>
+        </p>
+        <p>
+          Rather than being a noisy social platform, it focuses on providing a{' '}
+          <b className="font-semibold text-inter text-foreground">
+            quiet place to write, think, and share meaningful content.
+          </b>
+        </p>
+      </section>
+
+      <section>
+        <h3>Our Principles</h3>
+        <p>Diary is built around a few simple priciples: </p>
+        <ul>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Simplicity
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Privacy
+              </b>
+              - capture ideas, thoughts, or daily experiences
+            </p>
+          </li>
+          <li>
+            <p>
+              <b className="font-semibold text-inter text-foreground">
+                Freedom to Share
+              </b>
+              - you decide whether your notes stay private or become public
+            </p>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Community</h3>
+        <p>
+          Diary encourages a community where people can{' '}
+          <b className="font-semibold text-inter text-foreground">
+            share experiences, ideas, and reflections.
+          </b>
+        </p>
+        <p>
+          Published notes allow users to connect through stories, thoughts, and
+          perspectives from others.
+        </p>
+      </section>
     </div>
   );
 };
